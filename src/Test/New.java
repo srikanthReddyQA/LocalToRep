cadc7b74ba0a58599185abda070da615e65a977d
srikanth <srikanthreddy.chilukuri@gmail.com>
2021-03-06 10:36:09 +0530

    test

--- src/Test/New.java
+++ src/Test/New.java
@@ -5,9 +5,11 @@ public class New {
 	    public static void main(String[] args) 
 	    { 
 	        String s1 = new String("HELLO"); 
-	        String s2 = new String("HELLO"); 
+	        String s2 = new String("HELLO");
+	        String s= new String("HELLO");
 	        System.out.println(s1 == s2); 
 	        System.out.println(s1.equals(s2)); 
+	        
 	    } 
 	}
 
